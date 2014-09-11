e25e553095a140ecce19d655f747e6513e9bef43
u49768 <u49768@a76b4225-d206-4d2b-a39c-a8ac0ed6cecc.(none)>
2014-09-11 06:27:45 +0000

    scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gitflow2TryoutApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
